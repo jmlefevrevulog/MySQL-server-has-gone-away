999c39beb6c2fbb2be500d4f4212d2e1aa40544c
jmlefevrevulog <42238482+jmlefevrevulog@users.noreply.github.com>
2021-01-25 11:58:30 +0100

    Update base.py

--- mysql_server_has_gone_away/base.py
+++ mysql_server_has_gone_away/base.py
@@ -26,7 +26,7 @@ def check_mysql_gone_away(db_wrapper):
                 # Map some error codes to IntegrityError, since they seem to be
                 # misclassified and Django would prefer the more logical place.
                 if e.args[0] in self.codes_for_integrityerror:
-                    raise base.utils.IntegrityError(*tuple(e.args))
+                    raise base.IntegrityError(*tuple(e.args))
                 raise
         return wrapper
 
